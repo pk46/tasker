61fdac3da8c9a77983c5c77baf0e0be634f2474d
pk46 <kupcik46@gmail.com>
2025-11-09 21:48:53 +0100

    Add models, services and API communication layer

---
+++ frontend/src/app/models/user.ts
@@ -0,0 +1,23 @@
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: "USER" | "ADMIN";
+}
+
+export interface UserCreate {
+  username: string;
+  email: string;
+  password: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+export interface UserUpdate {
+  email?: string;
+  password?: string;
+  firstName?: string;
+  lastName?: string;
+}
